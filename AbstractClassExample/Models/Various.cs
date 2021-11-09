2a1bd549f2fdc3e91980454fb589b0dd24827605
Gracie <90685469+Graciele89@users.noreply.github.com>
2021-11-09 17:51:53 +0000

    Add files via upload

---
+++ AbstractClassExample/Models/Various.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExample.Models
+{
+    public abstract class SuperHero
+    {
+    }
+
+    public abstract class Alien : SuperHero
+    {
+    }
+
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    interface ISeeThroughThings
+    {
+        void LookThroughObjects();
+    }
+
+    public class SuperMan : Alien, IFlyable, ISeeThroughThings
+    {
+        public void Fly()
+        {
+            //  throw new NotImplementedException();
+
+            Console.WriteLine("I defy gravity");
+        }
+
+        public void LookThroughObjects()
+        {
+            // throw new NotImplementedException();
+            Console.WriteLine("I use X-RAY Vision");
+        }
+    }
+}
+
---
+++ AbstractClassExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using AbstractClassExample.Models;
+using System;
+
+namespace AbstractClassExample
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+
+                //SuperHero superHero = new();
+
+                SuperMan superMan = new SuperMan();
+                superMan.Fly();
+                superMan.LookThroughObjects();
+
+                Sword sword = new Sword("BatSword");
+                sword.TakeDamage(5);
+            }
+        }
+   }
+
+
